0c3c10cac253e1e5e1048d643af5f3070be017ff
201RBC039 <125255543+201RBC039@users.noreply.github.com>
2023-03-05 23:31:39 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,43 +2,48 @@
 
 import sys
 import threading
-import numpy
+
 
 def compute_height(n, parents):
-    nodes = [[] for _ in range(n)]
+    children = [[] for _ in range(n)]
     for i in range(n):
         parent = parents[i]
         if parent == -1:
             root = i
         else:
-            nodes[parent].append(i)
-    
-    memo = {}
-    def height(node):
-        if node in memo:
-            return memo[node]
-        if not nodes[node]:
-            memo[node] = 0
-            return 0
-        h = max(height(child) for child in nodes[node]) + 1
-        memo[node] = h
-        return h
+            children[parent].append(i)
 
-input_type = input().strip()
-if input_type == 'F':
-    filename = input().strip()
-    if 'a' in filename:
-        print("Invalid filename!")
-        sys.exit()
-    with open("/home/runner/work/tree-height-from-empty-201RBC039/tree-height-from-empty-201RBC039/" + filename) as f:
-        n = int(f.readline())
-        parents = list(map(int, f.readline().split()))
-else:
-    n = int(input())
-    parents = list(map(int, input().split()))
+    def compute_depth(node):
+        if not children[node]:
+            return 1
+        max_depth = 0
+        for child in children[node]:
+            depth = compute_depth(child)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(root)
 
 
-#sys.setrecursionlimit(10**7)  
-#threading.stack_size(2**27)
-#threading.Thread(target=main).start()
-print(compute_height(n, parents))
+def main():
+    input_type = input()
+
+    if 'I' in input_type:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        height = compute_height(n, parents)
+        print(height)
+    elif 'F' in input_type:
+        filename = input()
+        with open("test/" + filename, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            height = compute_height(n, parents)
+            print(height)
+    else:
+        print("Invalids")
+        exit()
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
+threading.Thread(target=main).start()
