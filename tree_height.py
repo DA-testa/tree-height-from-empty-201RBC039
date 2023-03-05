63e86582964f25eddb61a336d4a23d8999c1dc9f
201RBC039 <125255543+201RBC039@users.noreply.github.com>
2023-03-05 12:37:52 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,28 +6,27 @@ import numpy
 
 
 def compute_height(n, parents):
-    node_children = {i: [] for i in range(n)}
-    
-    for i, parent in enumerate(parents):
-        if parent == -1:
-            root = i
-        else:
-            node_children[parent].append(i)
-    
+    adj = [[] for _ in range(n)]
+    for i in range(n):
+        if parents[i] != -1:
+            adj[parents[i]].append(i)
     
     def height(node):
-        if not node_children[node]:
+        if not adj[node]:
             return 1
-        else:
-            return 1 + max(height(child) for child in node_children[node])
+        return 1 + max(height(child) for child in adj[node])
     
-    return height(root)
+    return height(parents.index(-1))
+
 
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
 
+
 if __name__ == '__main__':
-    sys.setrecursionlimit(10**7)
+    sys.setrecursionlimit(10**7)  
+    threading.stack_size(2**27)
+    threading.Thread(target=main).start()
     main()
