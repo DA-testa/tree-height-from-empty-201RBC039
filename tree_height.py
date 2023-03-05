3b724d9b8a2cfcbcc38f882118ba2f5530738ee1
201RBC039 <125255543+201RBC039@users.noreply.github.com>
2023-03-05 13:21:27 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -5,25 +5,49 @@ import threading
 import numpy
 
 def compute_height(n, parents):
-    depths = [0] * n
+    tree = {}
     for i in range(n):
-        depth = 0
-        j = i
-        while j != -1:
-            if depths[j] != 0:
-                depth += depths[j]
-                break
-            depth += 1
-            j = parents[j]
-        depths[i] = depth
-    return max(depths)
+        tree[i] = []
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            tree[parent].append(i)
+
+    def height(node):
+        if not tree[node]:
+            return 1
+        else:
+            return max(height(child) for child in tree[node]) + 1
+
+    return height(root)
+
 
 def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
-    height = compute_height(n, parents)
-    print(height)
+    input_type = input("Enter input type (K for keyboard, F for file): ")
+    if input_type.upper() == 'K':
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif input_type.upper() == 'F':
+        filename = input("Enter file name: ")
+        if 'a' in filename.lower():
+            print("Invalid file name")
+            return
+        try:
+            with open('input/'+filename, 'r') as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+        except:
+            print("Invalid file name")
+            return
+    else:
+        print("Invalid input type")
+        return
+    print(compute_height(n, parents))
 
-sys.setrecursionlimit(10**7)
+
+sys.setrecursionlimit(10**7)  
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
+main()
